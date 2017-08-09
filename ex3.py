4454d7c5360f1369c047156d340542a04cab0ea9
Allison Harris <allisonharris@Allisons-MacBook-Pro.local>
2017-08-09 16:29:02 -0500

    Complete exercise 3

---
+++ ex3.py
@@ -0,0 +1,36 @@
+print "I will now count my chickens:"
+
+# 25 plus (30 divided by 6)
+print "Hens", 25 + 30 / 6
+
+#(100 minus (25 times 3)) mod 4
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+# (3 plus 2 plus 1 minus 5 plus (4 mod 2) minus (1 divided by 4) plus 6
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# test if 3 plus 2 is less than 5 minus 7
+print 3 + 2 < 5 - 7
+
+# add 3 and 2
+print "What is 3 + 2?", 3 + 2
+
+#subtract 7 from 5
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+# test if 5 is greater than -2
+print "Is it greater?", 5 > -2
+
+#test if 5 is greater than or equal to -2
+print "Is it greater or equal?", 5 >= -2
+
+#test if 5 is less than or equal to -2
+print "Is it less or equal?", 5 <= -2
