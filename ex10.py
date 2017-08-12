a31e7f559119d23614e17e1aed0216a8f07d6f5a
Allison Harris <allison.harris23@gmail.com>
2017-08-12 16:21:17 -0500

    Combine escape sequences and format strings to create more complex format

--- ex10.py
+++ ex10.py
@@ -1,14 +1,12 @@
-tabby_cat = "\tI'm tabbed in."
-persian_cat = "I'm split\non a line."
-backslash_cat = "I'm \\ a \\ cat."
-
-fat_cat = '''
-I'll do a list:
-\t* Cat food
-\t* Fishies
-\t* Catnip\n\t* Grass
+cats = '''
+\tI'm tabbed in.
+I'm split\non a line.
+I'm \\ a \\ cat.
+\nI'll do a list:
+\n\t*Cat food
+\n\t*Fishies
+\n\t*Catnip
+\n\t*Grass
 '''
 
-print tabby_cat + persian_cat 
-print backslash_cat + fat_cat
-
+print cats
