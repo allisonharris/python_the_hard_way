db1b2fc335c42fec8b6df5b075f89e299128a2c7
Allison Harris <allison.harris23@gmail.com>
2017-08-16 18:49:26 -0500

    Complete exercise 22

---
+++ ex22.py
@@ -0,0 +1,55 @@
+#  This is a list of every character I have used thus far in the LPTHW course
+
+# = comment. This is a way to leave more info about the code
+
+print will output some string to the screen
+	' ' or " " should be placed around strings and content to be printed
+
+""" """  is for multiline comments
+
+_ underscore acts as a space between words in a variable 
+
+True boolean value of true
+False boolean value of false
+
+= assign a variable
++ add values
+- subtract values
+* multiply values
+** exponent 
+/ divide values
+% modulus, returns the remainder of division
+> greater than
+< less than
+>= greater than or equal to
+<= less than or equal to
+== equals
++= incrementer (for instance x = x + y can be written as x += y)
+
+% for string interpolation
+%r interpolation of raw inputs (often used for debugging)
+%s interpolation of strings
+%d interpolation of numbers
+
+
+\n - creates a new line
+\' - escape out of a single quote
+\" - escape out of a double quote
+\\ - backslash
+\t - creates a tab
+
+raw_input() - this is a command prompt which takes input from the user
+
+
+from sys import argv # improts argv module from sys lib
+from os.path import exists # imports exists module from os.path
+
+
+read() # read file
+write() # write to file
+close() # close file
+truncate() # truncate file
+exists() # check if something exits 
+int() # convert strings and numbers to integers
+open() # open file
+len() # get the length of an input
