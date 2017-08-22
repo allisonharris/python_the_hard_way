9f0acda8d43ee1588454c4ed9d78e52ec5f11dba
Allison Harris <allison.harris23@gmail.com>
2017-08-22 12:38:45 -0500

    Change wording of gold_room

--- ex35.py
+++ ex35.py
@@ -7,7 +7,7 @@ def gold_room():
 	if "0" in choice or "1" in choice:
 		how_much = int(choice)
 	else:
-		dead("Man, learn to type a number.")
+		dead("Please enter a number.")
 
 	if how_much < 50:
 		print "Nice, you're not greedy, you win!"
